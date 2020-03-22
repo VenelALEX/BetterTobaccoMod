6ad6e55badf5a77f0c1395502123cc3113936242
Alexander Brenner <a.reinhardbrenner@gmail.com>
2020-03-22 16:05:08 +0100

    Added Items + lang files

---
+++ src/main/java/venelalex/bettertobacco/item/DriedTobacco.java
@@ -0,0 +1,12 @@
+package venelalex.bettertobacco.item;
+
+import net.minecraft.item.Item;
+import venelalex.bettertobacco.init.BetterTobaccoItemGroups;
+
+public class DriedTobacco extends Item{
+
+	public DriedTobacco() {
+		super(new Item.Properties().group(BetterTobaccoItemGroups.my_Item_Group));
+		// TODO Auto-generated constructor stub
+	}
+}
