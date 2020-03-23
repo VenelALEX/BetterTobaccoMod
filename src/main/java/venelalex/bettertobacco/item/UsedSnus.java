982a688683a73ce7a2a142c2d77b2f9155ec44db
Alexander Brenner <a.reinhardbrenner@gmail.com>
2020-03-23 17:48:12 +0100

    Added Snus-concept and tobacco ingot

---
+++ src/main/java/venelalex/bettertobacco/item/UsedSnus.java
@@ -0,0 +1,12 @@
+package venelalex.bettertobacco.item;
+
+import net.minecraft.item.Item;
+import venelalex.bettertobacco.init.BetterTobaccoItemGroups;
+
+public class UsedSnus extends Item{
+
+	public UsedSnus() {
+		super(new Item.Properties().group(BetterTobaccoItemGroups.my_Item_Group));
+		// TODO Auto-generated constructor stub
+	}
+}
